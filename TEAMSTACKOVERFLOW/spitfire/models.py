3f1214955cfb4dfbba50a3133ead53e94a1e8381
J-Mike <jamesmichael@umass.edu>
2017-10-17 14:04:12 -0400

    set up barebones for models

--- TEAMSTACKOVERFLOW/spitfire/models.py
+++ TEAMSTACKOVERFLOW/spitfire/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Track(models.Model):
+    """
+    Model for 
+    """
+
+    
+class Lyrics(models.Model):
+    """
+    Model for 
+    """
+
+    
+class Genre(models.Model):
+    """
+    Model for 
+    """
+
+    
+class Artist(models.Model):
+    """
+    Model for 
+    """
+
+    
+class Comment(models.Model):
+    """
+    Model for 
+    """
+
+    
