a344d98704d88e740b7a5577994edf13dc0ccca8
Ri Neng Huang <rineng0810@gmail.com>
2017-12-01 14:12:16 -0500

    started syncing lyrics with audio, far from done

---
+++ TEAMSTACKOVERFLOW/spitfire/static/javascript/sync.js
@@ -0,0 +1,30 @@
+//delete all the console logs, printing out for testing purpose
+/*
+$('#audio')[0].onplay = function(){
+	var audioElement = $('#audio')[0];
+	audioElement.addEventListener("loadedmetadata", function(_event) {
+    var duration = audioElement.duration;
+    console.log(duration);
+	var height = $('.card-text').css('height');
+	console.log(height);
+	var speed = parseInt(height)/parseInt(duration);
+	console.log(speed);
+	$('.card-text').hide().slideDown(speed*1000);
+	});
+}
+*/
+
+
+$('.card-title').on('click', function(){
+	var audioElement = $('#audio')[0];
+	audioElement.play();
+	//audioElement.addEventListener("loadedmetadata", function(_event) {
+    var duration = audioElement.duration;
+    console.log(duration);
+    var lyric = $(this).next().next();
+	var height = $(this).next().next().css('height');
+	console.log(height);
+	var speed = parseInt(height)/parseInt(duration);
+	console.log(speed);
+	lyric.hide().slideDown(speed*1000);
+});
